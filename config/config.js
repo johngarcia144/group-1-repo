9f0e140a4866337a97b6da6f8891b91314956f4e
wuzzlefump <paulelliottwork@gmail.com>
2020-09-28 00:03:53 -0600

    config is pretty important

--- config/config.js
+++ config/config.js
@@ -1,9 +1,11 @@
-{
+require('dotenv').config();
+
+module.exports={
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "passport_demo",
-    "host": "127.0.0.1",
+    "username": process.env.MYSQL_USER,
+    "password": process.env.MYSQL_KEY,
+    "database": process.env.MYSQL_DBNAME,
+    "host": process.env.MYSQL_HOST,
     "dialect": "mysql"
   },
   "test": {
