1f65e4c068baf59b916e5184580d958c8e283705
Dr Phil <philip.prewitt@gmail.com>
2020-09-30 16:06:06 -0600

    separated js routes and front end apis to resolve errors

--- public/js/members.js
+++ public/js/members.js
@@ -3,49 +3,5 @@ $(document).ready(() => {
   // and updates the HTML on the page
   $.get("/api/user_data").then(data => {
     $(".member-name").text(data.username);
-
-
-
-
   });
-
-
-  let editor = ace.edit("editor");
-  editor.setTheme("ace/theme/tomorrow_night");
-  editor.session.setMode("ace/mode/xml");
-  editor.session.setUseSoftTabs(true);
-
-  //toggles mode
-  $("#languageSelect").on("change", event =>{
-    event.preventDefault();
-    let mode = $("#languageSelect").val()
-    editor.session.setMode(`ace/mode/${mode}`);
-    console.log("toggle");
-  });
-
-  //captures inputs for new code
-  $("#savecode").on("click", event => {
-    event.preventDefault();
-    const Code = {
-      snip: editor.getValue(),
-      codeType: $("#languageSelect").val(),
-      title: $("#title").val().trim(),
-      tags: $("#addKeywords").val().trim(),
-      public: $("#privateSelect").val().trim()
-    };
-    console.log(Code);
-    //add post api here
-  });
-
-  //searches database for language and keyword
-  $("#searchcode").on("click", event => {
-    event.preventDefault();
-    const searchParams = {
-      codeType: $("#languageSearch").val(),
-      tags: $("#searchtag").val().trim()
-    };
-    console.log(searchParams);
-    //add get api here
-  });
-
 });
