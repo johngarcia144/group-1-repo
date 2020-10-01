7d2467fea0ae1aa8a17632caf67c1ac639e2f36f
Dr Phil <philip.prewitt@gmail.com>
2020-10-01 10:27:45 -0600

    correcting all linter alerts

--- public/js/sessions.js
+++ public/js/sessions.js
@@ -1,47 +1,52 @@
 $(document).ready(() => {
-  let editor = ace.edit("editor");
+  const editor = ace.edit("editor");
   editor.setTheme("ace/theme/tomorrow_night");
   editor.session.setMode("ace/mode/xml");
   editor.session.setUseSoftTabs(true);
   //toggles mode
-  $("#languageSelect").on("change", event =>{
+  $("#languageSelect").on("change", event => {
     event.preventDefault();
-    let mode = $("#languageSelect").val()
+    const mode = $("#languageSelect").val();
     editor.session.setMode(`ace/mode/${mode}`);
     console.log("toggle");
-});
+  });
 
-//captures inputs for new code
+  //captures inputs for new code
   $("#savecode").on("click", event => {
     event.preventDefault();
     const publicStr = $("#privateSelect").val();
-    console.log("click")
+    console.log("click");
     const Code = {
       snip: editor.getValue(),
       codeType: $("#languageSelect").val(),
-      title: $("#title").val().trim(),
-      tags: $("#addKeywords").val().trim(),
+      title: $("#title")
+        .val()
+        .trim(),
+      keywords: $("#addKeywords")
+        .val()
+        .trim(),
       public: parseInt(publicStr)
     };
     $.post("/api/new/", Code).then(data => {
       console.log(data);
     });
-  })
+  });
 
-    //searches database for language and keyword
-    $("#searchcode").on("click", event => {
-        event.preventDefault();
-        const searchParams = {
-            codeType: $("#languageSearch").val(),
-            keywords: $("#searchtag").val().trim()
-        };
+  //searches database for language and keyword
+  $("#searchcode").on("click", event => {
+    event.preventDefault();
+    const searchParams = {
+      codeType: $("#languageSearch").val(),
+      keywords: $("#searchtag")
+        .val()
+        .trim()
+    };
 
-        $.get(`/api/codes/search/${searchParams.keywords}`)
-
-            // on success, run this callback
-            .then(searchParams => {
-                // log the data we found
-                console.log(searchParams);
-            });
-    });
+    $.get(`/api/codes/search/${searchParams.keywords}`)
+      // on success, run this callback
+      .then(searchParams => {
+        // log the data we found
+        console.log(searchParams);
+      });
+  });
 });
--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const exphbs = require("express-handlebars");
 // Requiring passport as we've configured it
 const passport = require("./config/passport");
 
-
 // Setting up port and requiring models for syncing
 const PORT = process.env.PORT || 8080;
 const db = require("./models");
@@ -16,8 +15,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-app.engine("handlebars",exphbs({ defaultLayout: "main"}));
-app.set("view engine", "handlebars")
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 // We need to use sessions to keep track of our user's login status
 app.use(
   session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
