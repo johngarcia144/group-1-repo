e4a055a69c45e026c0c628b83d03cfb54e80da6a
Cat Lui <65638648+catherinelui2@users.noreply.github.com>
2020-10-02 20:32:34 -0600

    Revert "problem with this code, update and delete buttons not populating, use…"

--- public/js/sessions.js
+++ public/js/sessions.js
@@ -57,33 +57,25 @@ $(document).ready(() => {
     $.get(`/api/codes/search/${searchParams.keywords}`)
       // on success, run this callback
       .then(response => {
-        // console.log("thing", response);
+        console.log("thing", response);
         // log the data we found
         $("#searchResults").empty();
         for (let i = 0; i < response.length; i++) {
-          if (
-            response[i].public == 1 &&
-            response[i].codeType === searchParams.codeType
-          ) {
+          if (response[i].public == 1 && response[i].codeType === searchParams.codeType) {
             const a = $(
               "<br><button class= 'btn-outline-primary mb-1 mt-2 btn d-flex justify-content-center btn-default btn-block'>"
             );
-            a.addClass("snips resultsbtn");
+            a.addClass("snips");
             a.attr("id", response[i].id);
-            a.attr("userId", response[i].userId);
             a.text(response[i].title);
             $("#searchResults").prepend(a);
           }
         }
         $("#searchResults").on("click", e => {
           console.log(e.target.id);
-          console.log(e.target.userid);
           e.preventDefault();
-          if (e.target.userId == u) {
-            updateDeleteBtn();
-          }
           for (let j = 0; j < response.length; j++) {
-            // console.log(response[j].id);
+            console.log(response[j].id);
             if (e.target.id == response[j].id) {
               const codeSnip = response[j].snip;
               editor.setValue(codeSnip);
@@ -93,13 +85,11 @@ $(document).ready(() => {
               const mode = $("#languageSelect").val();
               editor.session.setMode(`ace/mode/${mode}`);
               console.log(response[j].public);
-              // console.log(typeof response[j].public);
+              console.log(typeof response[j].public);
               if (response[j].public == true) {
-                // console.log("TRUE!");
+                console.log("TRUE!")
                 $("#privateSelect").val("1");
-              } else {
-                $("#privateSelect").val("0");
-              }
+              } else $("#privateSelect").val("0");
             }
           }
         });
@@ -127,9 +117,8 @@ $(document).ready(() => {
             const a = $(
               "<br><button class= 'btn-outline-primary mb-1 mt-2 btn d-flex justify-content-center btn-default btn-block'>"
             );
-            a.addClass("snips resultsbtn");
+            a.addClass("snips");
             a.attr("id", response[i].id);
-            a.attr("userId", response[i].userId);
             a.text(response[i].title);
             $("#personalcontainer").prepend(a);
           }
@@ -137,7 +126,6 @@ $(document).ready(() => {
         $("#personalcontainer").on("click", e => {
           console.log(e.target.id);
           e.preventDefault();
-          updateDeleteBtn();
           for (let j = 0; j < response.length; j++) {
             console.log(response[j].id);
             if (e.target.id == response[j].id) {
@@ -148,22 +136,4 @@ $(document).ready(() => {
         });
       });
   });
-  //make delete/update buttons when userId equals user
-  function updateDeleteBtn() {
-    const deletebtn = $(
-      "<br><button class= 'btn-outline-primary mb-1 mt-2 btn d-flex justify-content-center btn-default btn-block'>"
-    );
-    deletebtn.attr("id", e.target.id);
-    deletebtn.attr("userId", e.target.userId);
-    deletebtn.addClass("delete");
-    deletebtn.text("Delete Code Snip");
-    const updatebtn = $(
-      "<br><button class= 'btn-outline-primary mb-1 mt-2 btn d-flex justify-content-center btn-default btn-block'>"
-    );
-    updatebtn.attr("id", e.target.id);
-    updatebtn.attr("userId", e.target.userId);
-    updatebtn.addClass("update");
-    deletebtn.text("Update Code Snip");
-    $(".buttonappend").append(deletebtn, updatebtn);
-  }
 });
